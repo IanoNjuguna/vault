57014133b34bd0f7a7e310e5f6cb9502bce73e8b
iano <inc3099@gmail.com>
2024-11-05 09:28:10 +0300

    a CLI calculator

---
+++ 06-go/03-tiny_projects/04-cmd_calc/cmd_calc.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num1, num2 float64
+	var operator string
+
+	fmt.Println("This is a CLI calculator")
+	fmt.Print("Enter the first number: ")
+	_, err := fmt.Scanln(&num1)
+	if err != nil {
+		fmt.Println("Invalid Input")
+		return
+	}
+	fmt.Print("Enter the Operator (+, -, / or *): ")
+	fmt.Scanln(&operator)
+	fmt.Print("Enter the second number: ")
+	_, err = fmt.Scanln(&num2)
+	if err != nil {
+		fmt.Println("Invalid Input")
+		return
+	}
+
+	switch operator {
+	case "+":
+		fmt.Printf("%.2f + %.2f = %.2f\n", num1, num2, num1+num2)
+	case "-":
+		fmt.Printf("%.2f - %.2f = %.2f\n", num1, num2, num1-num2)
+	case "*":
+		if num1 == 0 || num2 == 0 {
+			fmt.Println("Error: Cannot multiply by zero.")
+		} else {
+			fmt.Printf("%.2f * %.2f = %.2f\n", num1, num2, num1*num2)
+		}
+	case "/":
+		if num1 == 0 || num2 == 0 {
+			fmt.Println("Error: Cannot divide by zero.")
+		} else {
+			fmt.Printf("%.2f / %.2f = %.2f\n", num1, num2, num1/num2)
+		}
+	default:
+		fmt.Println("You provided an invalid operator. Provide either +, *, - or/")
+	}
+}
---
+++ 06-go/03-tiny_projects/04-cmd_calc/go.mod
@@ -0,0 +1,3 @@
+module cmd_calculator
+
+go 1.23.2
