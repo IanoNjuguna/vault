3a83a491c1e0dc22e4266cca6e5db98990314264
iano <inc3099@gmail.com>
2024-11-13 10:56:27 +0300

    a palindrome checker

---
+++ 06-go/03-tiny_projects/08-palindrome_checker/palindrome_chckr.go
@@ -0,0 +1,45 @@
+package main
+
+/**
+ * A palindrome is a word, number, phrase, or other sequence of symbols
+ * that reads the same backwards as forwards, such as:
+ * madam or racecar,
+ * the date "22/02/2022",
+ * and the sentence: "A man, a plan, a canal – Panama".
+ */
+
+import (
+	"fmt"
+)
+
+/**
+ * reverse_wrd - reverses word to
+ * 		check whether the result
+ *		is the same as original input.
+ *
+ * @word: original input
+ */
+
+func reverse_wrd(word string) string {
+	reversed := ""
+
+	for i := (len(word) - 1); i >= 0; i-- {
+		reversed += string(word[i])
+	}
+
+	return reversed
+}
+
+func main() {
+	var word string
+	fmt.Print("Enter the word to check: ")
+	fmt.Scanln(&word)
+
+	reversed := reverse_wrd(word)
+
+	if reversed == word {
+		fmt.Printf("%s is a palindrome\n", word)
+	} else {
+		fmt.Printf("%s is not a palindrome\n", word)
+	}
+}
