ad15e31f59b81a7b3f425c6d2fb87cdeae9bfcd0
iano <inc3099@gmail.com>
2024-12-30 14:48:42 +0300

    Hello World Function

---
+++ 0000-leetcode_2025/30_days_of_JS/00-hello_world.js
@@ -0,0 +1,11 @@
+/**
+ * Create a Hello World Function
+ * Should return a new function that always returns the string, "Hello World"
+ */
+
+var createHelloWorld = function() {
+	return function greet() {
+		return "Hello World";
+	};
+};
+
